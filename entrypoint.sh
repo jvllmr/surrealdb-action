9e4276c0498288e8dce566332dd813e576588b17
Jan Vollmer <jan@vllmr.dev>
2022-09-15 22:08:45 +0200

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VERSION=$1
+PORT=$2
+
+if [ -z "$VERSION" ]; then
+  VERSION='latest'
+fi
+echo "SurrealDB version: $VERSION - SurrealDB Port: $PORT"
+echo "Starting SurrealDB"
+docker run --name surrealdb --publish $PORT:8000 --detach surrealdb/surrealdb:$VERSION
